ad7ccb7c19a123572a967facd7c7e150de3c7eb2
VTS1599 <vts1599@gmail.com>
2024-12-31 23:57:19 +0530

    Created services and changes accordingly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { SkeletonComponent } from './skeleton/skeleton.component';
+import { FormsModule } from '@angular/forms';
+import { GeminiService } from './gemini.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    SkeletonComponent
+    SkeletonComponent,
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'AiForAll';
+
+  prompt: string = '';
+
+  geminiService: GeminiService = inject(GeminiService);
+
+  loading: boolean = false;
+
+  chatHistory: any[] = [];
+
+  constructor() {
+    this.geminiService.getMessageHistory().subscribe((res) => {
+      if(res) {
+        this.chatHistory.push(res);
+      }
+    })
+  }
+
+  async sendData(){
+    if(this.prompt && !this.loading){
+      this.loading = true;
+      const data = this.prompt;
+      this.prompt = '';
+      await this.geminiService.generateText(data);
+      this.loading = false;
+    }
+  }
+
+  formatText(text: string){
+    const result = text.replaceAll('*', '');
+    return result;
+  }
 }
